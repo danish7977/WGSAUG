919b596cc0cab8c685a5ae8d8ff18efc4f71dc01
danish00 <memondanish710000@gmail.com>
2022-08-18 16:14:50 +0530

    Math and Cal

---
+++ src/module-info.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class Calculator {
+	public void ad(int a, int b) {
+		System.out.println("Sum is :" + a+b);
+		
+	}
+public void add(int a,int b,int c) {
+	System.out.println("Sum is :" +a+b+c);
+	
+	
+}
+public void multiply(int a, int b) {
+	System.out.println("Product is" +a*b);
+}
+}
